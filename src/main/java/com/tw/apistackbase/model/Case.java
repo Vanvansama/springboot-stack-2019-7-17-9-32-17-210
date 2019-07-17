5d1809f3a249da81784e68bfa9ee36ac454a2d69
Kamisama <953181215@qq.com>
2019-07-17 21:06:12 +0800

    [stefan] finish Story1-AC1

---
+++ src/main/java/com/tw/apistackbase/model/Case.java
@@ -0,0 +1,25 @@
+package com.tw.apistackbase.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Create with IDEA
+ *
+ * @Author: gitee.com/KamisamaXX
+ * @Date: Create in 20:43 2019/7/17
+ * @Description:
+ */
+@Entity
+@Table(name = "Case")
+public class Case {
+    @Id
+    @GeneratedValue
+    private String id;
+
+    @Column(name = "case_name", nullable = false)
+    private String caseName;
+
+    @Column(name = "date", nullable = false)
+    private Long date;
+}
